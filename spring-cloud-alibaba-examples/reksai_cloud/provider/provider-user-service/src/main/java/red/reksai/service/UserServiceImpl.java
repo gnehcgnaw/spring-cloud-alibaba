0aca15256b5828ed915f5a6d5e5e8cb2f89df6d7
gnehcgnaw <949174604@qq.com>
2019-02-11 01:06:52 +0800

    dubbo的rest暴露服务方式的添加

--- spring-cloud-alibaba-examples/reksai_cloud/provider/provider-user-service/src/main/java/red/reksai/service/UserServiceImpl.java
+++ spring-cloud-alibaba-examples/reksai_cloud/provider/provider-user-service/src/main/java/red/reksai/service/UserServiceImpl.java
@@ -3,15 +3,20 @@ package red.reksai.service;
 import com.alibaba.dubbo.config.annotation.Service;
 import service.IUserService;
 
+import javax.ws.rs.*;
+
 /**
  * @author : gnehcgnaw
  * @since : 2019-02-10 21:54
  */
 @Service(version = "0.0.1")
+@Path("user")
 public class UserServiceImpl  implements IUserService {
 
+    @GET
+    @Path("/add")
     @Override
-    public int add(int x, int y){
+    public int add(@QueryParam("x") int x, @QueryParam("y") int y){
         return x+y;
     }
 }
