bd8bc09e7eb0d5000ca96e8e7af7559ea1420470
gnehcgnaw <949174604@qq.com>
2019-02-15 22:20:05 +0800

    修改user-api

--- spring-cloud-alibaba-examples/reksai_cloud/api/user-api/src/main/java/entity/User.java
+++ spring-cloud-alibaba-examples/reksai_cloud/api/user-api/src/main/java/entity/User.java
@@ -1,9 +1,22 @@
 package entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.Version;
+import lombok.Data;
+
 /**
  * @author : gnehcgnaw
  * @since : 2019-02-10 14:59
  */
+@Data
 public class User {
-
+    @TableId(type = IdType.ID_WORKER)
+    private Long id;
+    private String name;
+    private Integer age;
+    private String email;
+    private Boolean isUsing;
+    @Version
+    private Integer version ;
 }
--- spring-cloud-alibaba-examples/reksai_cloud/api/user-api/src/main/java/service/IUserService.java
+++ spring-cloud-alibaba-examples/reksai_cloud/api/user-api/src/main/java/service/IUserService.java
@@ -1,9 +1,13 @@
 package service;
 
+import entity.User;
+
 /**
  * @author : gnehcgnaw
  * @since : 2019-02-10 21:55
  */
 public interface IUserService {
     public int add(int x,int y);
+
+    User delUser(Integer id);
 }
