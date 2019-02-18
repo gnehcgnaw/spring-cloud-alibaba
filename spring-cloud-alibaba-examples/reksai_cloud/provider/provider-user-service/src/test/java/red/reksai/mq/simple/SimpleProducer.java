ae469f9b9c6ec8f06619ef44b5813f5724b60f2a
gnehcgnaw <949174604@qq.com>
2019-02-18 16:36:42 +0800

    1.清除UserController中无效依赖； 2.修改rocketmq连接配置； 3.新增simple consumer和produce 。

---
+++ spring-cloud-alibaba-examples/reksai_cloud/provider/provider-user-service/src/test/java/red/reksai/mq/simple/SimpleProducer.java
@@ -0,0 +1,9 @@
+package red.reksai.mq.simple;
+
+/**
+ * @author : <a href="mailto:gnehcgnaw@gmail.com">gnehcgnaw</a>
+ * @date : 2019-02-18 09:56
+ * @since :
+ */
+public class SimpleProducer {
+}
