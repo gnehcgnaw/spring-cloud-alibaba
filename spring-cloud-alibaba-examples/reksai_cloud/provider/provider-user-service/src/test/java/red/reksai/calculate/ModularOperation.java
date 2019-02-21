6e6ef82d2cd0b34106ea25685e20c0853900d15a
gnehcgnaw <949174604@qq.com>
2019-02-21 14:25:09 +0800

    模运算测试

---
+++ spring-cloud-alibaba-examples/reksai_cloud/provider/provider-user-service/src/test/java/red/reksai/calculate/ModularOperation.java
@@ -0,0 +1,14 @@
+package red.reksai.calculate;
+
+/**
+ * @author : <a href="mailto:gnehcgnaw@gmail.com">gnehcgnaw</a>
+ * @date : 2019-02-20 16:45
+ * @since :
+ */
+public class ModularOperation {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i%10);
+        }
+    }
+}
